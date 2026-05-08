cb0bac27d21185ad3432d64283bc7c8bc33d994c
Rinit Bhowmick <96461521+rinit18@users.noreply.github.com>
2026-05-08 16:36:14 +0530

    Add: design-hashmap - submission-13

---
+++ Data Structures & Algorithms/design-hashmap/submission-13.java
@@ -0,0 +1,45 @@
+class MyHashMap {
+
+    private ArrayList<Integer[]> map;
+     public MyHashMap() {
+        map = new ArrayList<>();
+    }
+    public void put(int key, int value) {
+        for(Integer[] pair : map){
+            if(pair[0] == key){
+                pair[1] = value;
+                return;
+            }
+        }
+        map.add(new Integer[]{key, value});
+        
+    }
+    public int get(int key) {
+        
+        for(Integer[] pair : map){
+            if(pair[0] == key){
+                return pair[1];
+            }
+        }
+        return -1;
+    }
+    public void remove(int key) {
+        for(Integer[] pair : map){
+            if(pair[0] == key){
+                map.remove(pair);
+                return;
+            }
+
+        }
+
+    }
+
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
