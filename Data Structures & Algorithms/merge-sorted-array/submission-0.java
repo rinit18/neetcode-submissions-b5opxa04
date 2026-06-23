929d683d50adc94461da536235c329d4679c4948
Rinit Bhowmick <96461521+rinit18@users.noreply.github.com>
2026-06-23 18:46:27 +0530

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int p1 = m -1;
+        int p2 = n- 1;
+        int write = m+n -1;
+
+        while(p2>=0){
+
+            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+
+                nums1[write] = nums1[p1];
+
+                p1--;
+
+            } else{
+
+                nums1[write] = nums2[p2];
+
+                p2--;
+
+            }
+
+            write--;
+
+        }
+    }
+}
