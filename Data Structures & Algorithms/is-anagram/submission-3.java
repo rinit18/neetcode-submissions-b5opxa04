88482ef9efb7ee7f80f7a29a583db0738fdae9aa
Rinit Bhowmick <96461521+rinit18@users.noreply.github.com>
2026-07-29 11:55:13 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+     
+     
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chart);
+
+
+         if(Arrays.equals(chars, chart)){
+
+                    return true;
+                } 
+
+        return false;
+    }
+
+}
